bb37e4ebfe0d1db458bedac4841b02f0ddd87162
Samuel Gustafsson <samuelgustafsson890623@gmail.com>
2020-04-04 19:59:25 +0200

    add vue-router

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import startpage from "./components/startpage.vue";
+import missions from "./components/missions.vue";
+
+Vue.use(Router);
+
+export default  new Router({
+    mode:"history",
+    routes: [
+        { 
+            path: "/", 
+            name: "home", 
+            component: startpage 
+        },
+        { 
+            path: "/missions", 
+            name: "missions", 
+            component: missions 
+        },
+    ]
+})
+
+  
